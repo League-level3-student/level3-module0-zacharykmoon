385a824a9ef0cc43ca45ff1f2b5f234aa6518e14
league <league@imac-20.attlocal.net>
2023-10-13 18:23:31 -0700

    dwdwdwsdf

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -18,30 +18,40 @@ public class _01_IntroToArrays {
     				names[3]= "Stupid";
         // 4. print the third element again
     						System.out.println(names[3]);
+    						for(int i = 0; i < names.length; i++){
+    				            names[i]="hi";
+    				        }
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
     						for (String strings : names) {
     				    		//start here
-    				    		strings="hello";
-    				    	}
+    							System.out.println(i);				    	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-    						for(String i : names){
-    				            System.out.println(i);
-    				        }
+    						
         // 7. make an array of 50 integers
-    	int[] arr = new int[50];
-    	for(int i=0; i<intArray.length;i++)
-    	{
-    		int
+    	int[] fiftyInts = new int[50];
     	
-    	}
+    	Random ran = new Random();
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+    	for(int i=0; i<names.length;i++)
+    	{
+    		fiftyInts[i] = ran.nextInt(100);
+    		
+    	}
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+    	int smallest =fiftyInts[0];
+    	int largest = fiftyInts[0];
+    	for (int i = 0; i<fiftyInts.length; i++) {
+    		if (fiftyInts[i]<smallest) {
+    			smallest = fiftyInts[i];
+    		}
+    		if (fiftyInts[i]>largest) {
+    			largest = fiftyInts[i];
+    		}
+    	}
         // 10 print the entire array to see if step 8 was correct
 
         // 11. print the largest number in the array.
